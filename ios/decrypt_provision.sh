091c689a23e84f3ab8fca5075d3fcdb46b416747
pvikas241 <62456590+pvikas241@users.noreply.github.com>
2022-09-14 11:11:07 +0530

    Update decrypt_provision.sh

--- ios/decrypt_provision.sh
+++ ios/decrypt_provision.sh
@@ -5,4 +5,4 @@ mkdir secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="@*!Ext652" \
---output secrets/MYCEA_Development_Profile.mobileprovision MYCEA_Development_Profile.mobileprovision.gpg
+--output secrets/MYCEA_Development_Profile.mobileprovision.mobileprovision MYCEA_Development_Profile.mobileprovision.gpg
